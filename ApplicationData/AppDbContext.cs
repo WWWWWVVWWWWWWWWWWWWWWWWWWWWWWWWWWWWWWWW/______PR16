88d2d7fcc6e0df84dfafeb3a8e6dcc3915b004ce
u31102 <u31102@P31102>
2024-12-13 11:38:54 +1000

    Добавьте файлы проекта.

---
+++ ApplicationData/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR16.ApplicationData
+{
+    public class AppDbContext
+
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+
+    }
+
+        public class Product { /* Свойства из таблицы Products */ }
+        public class Category { /* Свойства из таблицы Categories */ }
+        public class Material { /* Свойства из таблицы Materials */ }
+        public class Color { /* Свойства из таблицы Colors */ }
+        public class Country { /* Свойства из таблицы Countries */ }
+
+
+    }
+
---
+++ ApplicationData/Prosucts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR16.ApplicationData
+{
+    public class Prosucts
+    {
+
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int CategoryID { get; set; }
+            public int MaterialID { get; set; }
+            public int ColorID { get; set; }
+            public int CountryID { get; set; }
+        
+    }
+}
